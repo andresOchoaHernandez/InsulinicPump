8015067bb078412825bbf7e2ea8803df2b2c61ed
andresOchoaHernandez <ada.wong.gv@gmail.com>
2022-01-25 17:14:30 +0100

    little refactoring

--- src/main/java/com/andreso/insulinicpump/device/hardware/PowerSupply.java
+++ src/main/java/com/andreso/insulinicpump/device/hardware/PowerSupply.java
@@ -2,6 +2,14 @@ package com.andreso.insulinicpump.device.hardware;
 
 public class PowerSupply extends DeviceComponent{
 
+    private int batteryLevel;
+
+    public PowerSupply(){
+        batteryLevel = 100;
+    }
+
+
+
     @Override
     public boolean selfTest() {
         return this.isDeviceWorkingProperly;
